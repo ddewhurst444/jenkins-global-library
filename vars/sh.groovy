63a785382c396df5133b34fcc0f2b91629ebde68
Dylan Dewhurst <39278992+ddewhurst444@users.noreply.github.com>
2019-03-08 15:23:29 -0700

    Create sh.groovy

---
+++ vars/sh.groovy
@@ -0,0 +1,22 @@
+def call(Map params = [:]) {
+    String script = params.script
+    Boolean returnStatus = params.get('returnStatus', false)
+    Boolean returnStdout = params.get('returnStdout', false)
+    String encoding = params.get('encoding', null)
+
+    timeout(time: 2, unit: HOURS) {
+        ansiColor('xterm') {
+            timestamps {
+                /* invoke the built-in sh step */
+                return steps.sh(script: script,
+                          returnStatus: returnStatus,
+                          returnStdout: returnStdout,
+                              encoding: encoding)
+            }
+        }
+    }
+}
+/* Convenience overload */
+def call(String script) {
+    return call(script: script)
+}
